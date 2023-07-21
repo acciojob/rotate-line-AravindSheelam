866a006e89ed39bee43d398003850251e7649f06
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-07-21 23:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let line=document.querySelector("#line");
+let angle=0;
+function rotate() {
+	angle+=1;
+	if(angle==360)
+		angle=0;
+	line.style.transform=`rotate(${angle}deg)`
+}
+setInterval(rotate,0);
